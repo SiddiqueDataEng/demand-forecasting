b0df295b4108cb12e13dab66091cb31f3e9fe28e
Siddique Data Engineer <dba.siddique@gmail.com>
2019-11-26 12:53:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+73 Demand Forecasting - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 73-demand-forecasting")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
